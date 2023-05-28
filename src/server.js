734926b03c0062a44e5c2ccc1bcfe21351ac35d7
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-05-28 09:48:59 +0700

    Implement MVC

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.post('/books');
+
 app.listen(PORT, () => {
   console.log(`Server is listening on localhost:${PORT}`);
 });
